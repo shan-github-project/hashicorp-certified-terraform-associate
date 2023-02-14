c55ed4b91f580feaad26f533a0c38a408731afc4
shan-github-project <svg30.zeeshan@gmail.com>
2023-02-15 02:33:18 +0530

    multiple provider

--- 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
@@ -1,17 +1,17 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.6"
+  required_version = "~> 1.3.8"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
 
 # Provider-1 for us-east-1 (Default Provider)
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   profile = "default"
 }
 
