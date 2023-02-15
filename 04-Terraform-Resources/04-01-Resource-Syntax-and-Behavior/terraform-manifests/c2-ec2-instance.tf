cfb6b8daaaebc122d21d24ab6a28ce9926fe3ca8
shan-github-project <svg30.zeeshan@gmail.com>
2023-02-16 02:23:13 +0530

    tVersion awsV

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -2,10 +2,10 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+  ami               = "ami-0cc87e5027adcdca8"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  availability_zone = "us-east-2a"
+  #availability_zone = "us-east-2b"
   tags = {
     "Name" = "web"
     #"tag1" = "Update-test-1"    
