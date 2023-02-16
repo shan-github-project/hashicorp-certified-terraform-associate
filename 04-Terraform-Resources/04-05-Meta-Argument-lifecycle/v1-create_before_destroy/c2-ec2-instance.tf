e1ef5a650b85cdb42823f769398b041873c767ae
shan-github-project <svg30.zeeshan@gmail.com>
2023-02-17 02:44:16 +0530

    AMI, tfvERS,awsVers

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
@@ -1,15 +1,15 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami               = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami               = "ami-0cc87e5027adcdca8" # Amazon Linux
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  availability_zone = "us-east-2a"
+  #availability_zone = "us-east-2b"
   tags = {
     "Name" = "web-1"
   }
- /*
-  lifecycle {
-    create_before_destroy = true
-  }
-*/
+  
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
+
 }
--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami           = "ami-0cc87e5027adcdca8" # Amazon Linux
   instance_type = "t2.micro"
   tags = {
     "Name" = "web-2"
